2ae92b4ea0fece0d8ca7607face81bd6fec4acdc
Osderos <barda.george@gmail.com>
2022-03-23 19:19:32 +0200

    display all categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,15 @@
 const Category = require("../models/category");
 
-exports.category_list = function (req, res) {
-  res.send("Not implemented: category list");
+exports.category_list = function (req, res, next) {
+  Category.find().exec(function (err, list_categories) {
+    if (err) {
+      return next(err);
+    }
+    res.render("category_list", {
+      title: "All Categories",
+      category_list: list_categories,
+    });
+  });
 };
 
 exports.category_detail = function (req, res) {
